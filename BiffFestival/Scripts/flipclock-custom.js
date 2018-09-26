ba818cb39de4e2b5a3f9fc5df4fbcbaf4b0d151f
Vlad Ichim <ichimvlad95@gmail.com>
2018-09-26 23:22:48 +0300

    Text changes and banner image

--- BiffFestival/Scripts/flipclock-custom.js
+++ BiffFestival/Scripts/flipclock-custom.js
@@ -1,7 +1,7 @@
 ﻿var clock;
 
 $(document).ready(function () {
-    var date = new Date(2018, 8, 22);
+    var date = new Date(2018, 9, 14);
     var now = new Date();
 
     var clock = $('.clock').FlipClock(date, {
